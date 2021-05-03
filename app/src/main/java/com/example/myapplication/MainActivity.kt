1410b1f2fce176523ad68aab31a7bf968a0c6017
Andres Ricardo Perez <riperezro@unal.edu.co>
2021-05-02 22:08:43 -0500

    tested Parking Filter

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.coroutines.await
 import com.example.myapplication.dataAccess.apolloClient
+import com.example.myapplication.service.MapInformation
 import com.example.myapplication.service.UserLogin
 import com.example.myapplication.type.ParkinglotuserAuthInput
 import com.google.android.gms.maps.model.LatLng
@@ -34,23 +35,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         lifecycleScope.launchWhenResumed {
-            val user = ParkinglotuserAuthInput(email = Input.optional("aaa"),password = "a")
-            val response = apolloClient.mutate(LoginMutation(email = "verstappen@redbull.com",password = "123456")).await()
-            Log.d("ParkingList", "LOGIN ${response?.data}")
-            if( response.data?.ath_login != null ){
-                val email = response.data!!.ath_login?.email
-                val type = UserLogin.userType( email )
-                Log.d("ParkingList", "TYPE $type")
-                if( type != null ){
-                    if( type == "client" ){
-                        val clientUser = UserLogin.getClientByEmail( email )
-                        Log.d("ParkingList", "CLIENT $clientUser")
-                    }else if( type == "parking" ){
-                        val clientUser = UserLogin.getParkingLotUserByEmail( email )
-                        Log.d("ParkingList", "PLU $clientUser")
-                    }
-                }
-            }
+            val output = MapInformation.GetAllParkingsByOwnerId( 9 )
+            Log.d("ParkingList", "Success $output")
         }
     }
 }
